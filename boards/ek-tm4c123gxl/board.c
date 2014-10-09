6a79e4594e07a291025af8dbdd3052dfcdad24b1
Benjamin Valentin <benjamin.valentin@volatiles.de>
2014-10-09 21:46:53 +0200

    add driverlib

--- boards/ek-tm4c123gxl/board.c
+++ boards/ek-tm4c123gxl/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 volatiles
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License v2.1. See the file LICENSE in the top level directory for more
@@ -7,46 +7,30 @@
  */
 
 /**
- * @ingroup     board_stm32f3discovery
+ * @ingroup     board_ek-tm4c123gxl
  * @{
  *
  * @file
- * @brief       Board specific implementations for the STM32F3Discovery evaluation board
+ * @brief       Board specific implementations for the EK-TM4C123GXL Launchpad evaluation board
  *
- * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Benjamin Valentin <benjamin.valentin@volatiles.de>
  *
  * @}
  */
 
 #include "board.h"
 
-#include "driverlib/gpio.h"
-#include "driverlib/pin_map.h"
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
 
 void board_init(void)
 {
-    //
-    // Set the clocking to run directly from the crystal.
-    //
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+    cpu_init();
 
-    /* set pendSV interrupt to lowest possible priority */
-    NVIC_SetPriority(PendSV_IRQn, 0xff);
-
-    //
-    // Enable the FPU
-    //
-    ROM_FPUEnable();
-
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
+    /* Enable the GPIO port that is used for the on-board LED */
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-    //
-    // Enable the GPIO pins for the LED (PF1, PF2 & PF3).
-    //
+    /* Enable the GPIO pins for the LED (PF1, PF2 & PF3) */
     ROM_GPIOPinTypeGPIOOutput(GPIOF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 }
