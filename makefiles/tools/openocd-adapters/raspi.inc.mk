853e13c3ddb5b08a02fe9641afe0bc1716afda55
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-03-29 18:17:20 +0100

    makefiles: add OpenOCD config for Raspberry Pi GPIO
    OpenOCD supports bit-banging the GPIOs of the Raspberry PI to emulate
    an SWD flasher.
    Add a raspi.inc.mk to support that option.

---
+++ makefiles/tools/openocd-adapters/raspi.inc.mk
@@ -0,0 +1,26 @@
+# Raspberry Pi GPIO
+
+export SWCLK ?= 21
+export SWDIO ?= 20
+export RST ?= 16
+
+ifeq (1, $(shell grep ARMv6 /proc/cpuinfo > /dev/null; echo $?))
+    # raspi2, 3
+    export PERIPH_BASE  = 0x3F000000
+    export SPEED_COEFF  = 146203
+    EXPORT SPEED_OFFSET = 36
+else
+    # raspi1
+    export PERIPH_BASE  = 0x20000000
+    export SPEED_COEFF  = 113714
+    export SPEED_OFFSET = 28
+endif
+
+export OPENOCD_ADAPTER_INIT ?= \
+  -c 'interface bcm2835gpio' \
+  -c 'bcm2835gpio_peripheral_base $(PERIPH_BASE)' \
+  -c 'bcm2835gpio_speed_coeffs $(SPEED_COEFF) $(SPEED_OFFSET)' \
+  -c 'bcm2835gpio_swd_nums $(SWCLK) $(SWDIO)' \
+  -c 'bcm2835gpio_srst_num $(RST)' \
+  -c 'reset_config srst_only srst_push_pull' \
+  -c 'transport select swd'
