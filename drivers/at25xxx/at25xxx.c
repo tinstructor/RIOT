437f50e53bff030f6e29e8d397bca33ec58b9ab0
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-08-28 13:54:55 +0200

    at25xxx: move command defines to separate file

--- drivers/at25xxx/at25xxx.c
+++ drivers/at25xxx/at25xxx.c
@@ -23,6 +23,7 @@
 #include <string.h>
 
 #include "at25xxx.h"
+#include "at25xxx_constants.h"
 #include "at25xxx_params.h"
 #include "byteorder.h"
 
@@ -38,19 +39,6 @@
 #define PAGE_SIZE   (AT25XXX_PARAM_PAGE_SIZE)
 #define ADDR_LEN    (AT25XXX_PARAM_ADDR_LEN)
 #define ADDR_MSK    ((1UL << ADDR_LEN) - 1)
-
-#define CMD_WREN    (0x6)   /* Write Enable             */
-#define CMD_WRDI    (0x4)   /* Write Disable            */
-#define CMD_RDSR    (0x5)   /* Read Status Register     */
-#define CMD_WRSR    (0x1)   /* Write Status Register    */
-#define CMD_READ    (0x3)   /* Read from Memory Array   */
-#define CMD_WRITE   (0x2)   /* Write to Memory Array    */
-
-#define SR_WIP      (0x01)  /* Write In Progress        */
-#define SR_WEL      (0x02)  /* Write Enable Latch       */
-#define SR_BP0      (0x04)  /* Block Protect 0          */
-#define SR_BP1      (0x08)  /* Block Protect 1          */
-#define SR_SRWD     (0x80)  /* Status Register Write Disable */
 
 static inline void getbus(const at25xxx_t *dev)
 {
