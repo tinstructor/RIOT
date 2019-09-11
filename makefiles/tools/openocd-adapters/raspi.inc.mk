ce9a93b1ea01b309b5deb4a90df60d690e8dc4bc
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-09-11 18:06:36 +0200

    fixup! makefiles: add OpenOCD config for Raspberry Pi GPIO

--- makefiles/tools/openocd-adapters/raspi.inc.mk
+++ makefiles/tools/openocd-adapters/raspi.inc.mk
@@ -1,25 +1,27 @@
-# Raspberry Pi GPIO
+# Raspberry Pi GPIO as debug adapter
 
-export SWCLK ?= 21
-export SWDIO ?= 20
-export RST ?= 16
+SWCLK ?= 21
+SWDIO ?= 20
+RST ?= 16
 
 ifeq (1, $(shell grep ARMv6 /proc/cpuinfo > /dev/null; echo $?))
     # raspi2, 3
-    export PERIPH_BASE  = 0x3F000000
-    export SPEED_COEFF  = 146203
-    export SPEED_OFFSET = 36
+    PERIPH_BASE  = 0x3F000000
+    SPEED_COEFF  = 146203
+    SPEED_OFFSET = 36
 else
     # raspi1
-    export PERIPH_BASE  = 0x20000000
-    export SPEED_COEFF  = 113714
-    export SPEED_OFFSET = 28
+    PERIPH_BASE  = 0x20000000
+    SPEED_COEFF  = 113714
+    SPEED_OFFSET = 28
 endif
 
-export OPENOCD_ADAPTER_INIT ?= \
+OPENOCD_ADAPTER_INIT ?= \
   -c 'interface bcm2835gpio' \
   -c 'bcm2835gpio_peripheral_base $(PERIPH_BASE)' \
   -c 'bcm2835gpio_speed_coeffs $(SPEED_COEFF) $(SPEED_OFFSET)' \
   -c 'bcm2835gpio_swd_nums $(SWCLK) $(SWDIO)' \
   -c 'bcm2835gpio_srst_num $(RST)' \
   -c 'transport select swd'
+
+export OPENOCD_ADAPTER_INIT
