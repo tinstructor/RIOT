8fd1a0d1b5c40f0199a81bef7b95d02cf669038b
Robbe Elsas <robbe.elsas@ugent.be>
2019-10-10 15:05:20 +0200

    Added first approach to experiment timing control

--- examples/timing_control/main.c
+++ examples/timing_control/main.c
@@ -37,17 +37,21 @@
 kernel_pid_t pid_tc;
 static char stack_tc[THREAD_STACKSIZE_MAIN];
 xtimer_ticks32_t last_wup_tc;
-xtimer_t phy_cfg_timer;
+xtimer_t trx_phy_cfg_timer;
+// xtimer_t if_phy_cfg_timer;
 
 static void *thread_tc_handler(void *arg)
 {
     (void) arg;
 
-    msg_t msg = {.type = TC_MSG_PHY_CFG};
+    msg_t msg;
+    msg_t msg_trx = {.type = TC_MSG_TRX_PHY_CFG};
+    // msg_t msg_if = {.type = TC_MSG_IF_PHY_CFG};
     msg_t msg_queue[8];
     msg_init_queue(msg_queue, 8);
 
-    xtimer_set_msg(&phy_cfg_timer, PHY_CFG_INTERVAL, &msg, thread_getpid());
+    xtimer_set_msg(&trx_phy_cfg_timer, PHY_CFG_INTERVAL, &msg_trx, thread_getpid());
+    // xtimer_set_msg(&if_phy_cfg_timer, EXP_INTERVAL, &msg_if, thread_getpid());
 
     while (1) {
         xtimer_periodic_wakeup(&last_wup_tc, TX_WUP_INTERVAL - IF_TX_OFFSET_US - PULSE_DURATION_US);
@@ -63,18 +67,35 @@ static void *thread_tc_handler(void *arg)
         gpio_clear(IF_TX_PIN);
         DEBUG("if tx off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
         if (msg_try_receive(&msg) == 1) {
-            last_wup_tc = xtimer_now();
-            gpio_set(TX_PHY_CFG_PIN);
-            DEBUG("tx phy cfg on: %"PRIu32"\n",xtimer_usec_from_ticks(last_wup_tc));
-            xtimer_periodic_wakeup(&last_wup_tc, PULSE_DURATION_US);
-            gpio_clear(TX_PHY_CFG_PIN);
-            DEBUG("tx phy cfg off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
-            gpio_set(RX_PHY_CFG_PIN);
-            DEBUG("rx phy cfg on: %"PRIu32"\n",xtimer_usec_from_ticks(last_wup_tc));
-            xtimer_periodic_wakeup(&last_wup_tc, PULSE_DURATION_US);
-            gpio_clear(RX_PHY_CFG_PIN);
-            DEBUG("rx phy cfg off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
-            xtimer_set_msg(&phy_cfg_timer, PHY_CFG_INTERVAL, &msg, thread_getpid());
+            switch (msg.type) {
+                case TC_MSG_TRX_PHY_CFG:
+                    last_wup_tc = xtimer_now();
+                    gpio_set(TX_PHY_CFG_PIN);
+                    DEBUG("tx phy cfg on: %"PRIu32"\n",xtimer_usec_from_ticks(last_wup_tc));
+                    xtimer_periodic_wakeup(&last_wup_tc, PULSE_DURATION_US);
+                    gpio_clear(TX_PHY_CFG_PIN);
+                    DEBUG("tx phy cfg off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
+                    gpio_set(RX_PHY_CFG_PIN);
+                    DEBUG("rx phy cfg on: %"PRIu32"\n",xtimer_usec_from_ticks(last_wup_tc));
+                    xtimer_periodic_wakeup(&last_wup_tc, PULSE_DURATION_US);
+                    gpio_clear(RX_PHY_CFG_PIN);
+                    DEBUG("rx phy cfg off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
+                    xtimer_set_msg(&trx_phy_cfg_timer, PHY_CFG_INTERVAL, &msg_trx, thread_getpid());
+                    break;
+                
+                // case TC_MSG_IF_PHY_CFG:
+                //     last_wup_tc = xtimer_now();
+                //     gpio_set(IF_PHY_CFG_PIN);
+                //     DEBUG("if phy cfg on: %"PRIu32"\n",xtimer_usec_from_ticks(last_wup_tc));
+                //     xtimer_periodic_wakeup(&last_wup_tc, PULSE_DURATION_US);
+                //     gpio_clear(IF_PHY_CFG_PIN);
+                //     DEBUG("if phy cfg off: %"PRIu32"\n", xtimer_usec_from_ticks(last_wup_tc));
+                //     xtimer_set_msg(&if_phy_cfg_timer, EXP_INTERVAL, &msg_if, thread_getpid());
+                //     break;
+
+                default:
+                    break;
+            }
         }
     }
     
