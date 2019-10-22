84d9db06a845186b5096cc2d5d2ea23446276343
Robbe Elsas <robbe.elsas@ugent.be>
2019-10-22 14:09:43 +0200

    Updated visualizer to take SIR into account

--- examples/interference/visualizer.py
+++ examples/interference/visualizer.py
@@ -12,23 +12,26 @@ script_dir = os.path.dirname(__file__)
 for file in glob.glob(os.path.join(script_dir,"*.csv")):
     file_list.append(file)
 
-REGEXP = re.compile(r"^.*_(?P<bytes>\d+)B.*_(?P<time>\d+)(?P<prefix>[UM]*)S\.csv$")
+REGEXP = re.compile(r"^.*_(?P<bytes>\d+)B.*_(?P<time>\d+)(?P<prefix>[UM]*)S.*_(?P<sir>-?\d{1,2})DB\.csv$")
 
 df = pd.read_csv(file_list[0], header=None)
 df.columns = ["TRX PHY","IF PHY","PRR"]
 if_phys = df["IF PHY"].unique()
 
-graph_info = {}
+bar_info = {}
+line_info = {}
 
 for csv_file in file_list:
     match = re.match(REGEXP, csv_file)
 
     payload_size = "B"
     offset = "S"
+    sir = "DB"
 
     if (match):
         payload_size = match.group("bytes") + payload_size
         offset = match.group("time") + match.group("prefix") + offset
+        sir = match.group("sir") + sir
 
         df = pd.read_csv(csv_file, header=None)
         df.columns = ["TRX PHY","IF PHY","PRR"]
@@ -37,21 +40,21 @@ for csv_file in file_list:
         for if_phy in if_phys:
             df_if_phy = df.loc[df["IF PHY"] == if_phy]
             
-            if (if_phy, payload_size not in graph_info):
-                graph_info.setdefault((if_phy, payload_size), {})
+            if (if_phy, payload_size, sir not in bar_info):
+                bar_info.setdefault((if_phy, payload_size, sir), {})
             
-            if (offset not in graph_info[if_phy, payload_size]):
-                graph_info[if_phy, payload_size].setdefault(offset, {})
+            if (offset not in bar_info[if_phy, payload_size, sir]):
+                bar_info[if_phy, payload_size, sir].setdefault(offset, {})
 
-            graph_info[if_phy, payload_size][offset].update(df_if_phy[["TRX PHY","PRR"]].set_index("TRX PHY")["PRR"].to_dict())
+            bar_info[if_phy, payload_size, sir][offset].update(df_if_phy[["TRX PHY","PRR"]].set_index("TRX PHY")["PRR"].to_dict())
 
     else:
         raise ValueError("CSV filename %s incorrectly formatted" % csv_file)
 
-for if_phy, payload_size in graph_info:
+for if_phy, payload_size, sir in bar_info:
     title = "Interference: " + if_phy + ", Payload: " + payload_size
-    graph_info[if_phy, payload_size] = dict(sorted(graph_info[if_phy, payload_size].items(), key=lambda x: x[0].lower()))
-    ax = pd.DataFrame(graph_info[if_phy, payload_size]).T.plot(kind='bar', figsize=(10,7))
+    bar_info[if_phy, payload_size, sir] = dict(sorted(bar_info[if_phy, payload_size, sir].items(), key=lambda x: x[0].lower()))
+    ax = pd.DataFrame(bar_info[if_phy, payload_size, sir]).T.plot(kind='bar', figsize=(10,7))
     ax.set_xlabel('Offset between TX and IF')
     ax.set_ylabel('Packet Reception Rate [%]')
     ax.set_ylim([0, 120])
@@ -59,4 +62,4 @@ for if_phy, payload_size in graph_info:
     for i in ax.patches:
         ax.text(i.get_x() + i.get_width() / 2, i.get_height()+1.2, str(round(i.get_height(),2)), fontsize=8, color='dimgrey', rotation=90, ha="center", va="bottom")
     plt.title(title)
-    plt.savefig(if_phy + "_" + payload_size + ".png")
+    plt.savefig(if_phy + "_" + payload_size + "_" + sir + ".png")
