94bc994d7545fb2749f22654ba16fa8a326f42db
Robbe Elsas <robbe.elsas@ugent.be>
2019-12-13 17:13:37 +0100

    Added notes to visualizer for better inderstanding

--- examples/interference/visualizer.py
+++ examples/interference/visualizer.py
@@ -16,13 +16,17 @@ REGEXP = re.compile(r"^.*_(?P<bytes>\d+)B.*_(?P<time>\d+)(?P<prefix>[UM]*)S.*_(?
 
 frame_list = []
 
-for filename in file_list:
-    foo = pd.read_csv(filename, header=None)
-    foo.columns = ["TRX PHY","IF PHY","PRR"]
-    frame_list.append(foo)
+# NOTE make a frame_list from all csv files with correctly formatted name
+for csv_file in file_list:
+    match = re.match(REGEXP, csv_file)
+    if (match):
+        foo = pd.read_csv(csv_file, header=None)
+        foo.columns = ["TRX PHY","IF PHY","PRR"]
+        frame_list.append(foo)
 
-frame = pd.concat(frame_list, axis=0, ignore_index=True)
-if_phys = frame["IF PHY"].unique()
+# NOTE concatenate all frames from frame_list into single frame before selecting
+# the IF PHY column and only keeping unique values in if_phys
+if_phys = pd.concat(frame_list, axis=0, ignore_index=True)["IF PHY"].unique()
 
 bar_info = {}
 line_info = {}
@@ -53,6 +57,8 @@ for csv_file in file_list:
             if (offset not in bar_info[if_phy, payload_size, sir]):
                 bar_info[if_phy, payload_size, sir].setdefault(offset, {})
 
+            # NOTE selects TRX PHY and PRR columns from df_if_phy and sets TRX PHY as index before converting the frame
+            # to a dict with key = TRX PHY and value = PRR and finally appending to bar_info[if_phy, payload_size, sir][offset]
             bar_info[if_phy, payload_size, sir][offset].update(df_if_phy[["TRX PHY","PRR"]].set_index("TRX PHY")["PRR"].to_dict())
 
             if (if_phy, payload_size, offset not in line_info):
@@ -61,13 +67,18 @@ for csv_file in file_list:
             if (sir not in line_info[if_phy, payload_size, offset]):
                 line_info[if_phy, payload_size, offset].setdefault(sir, {})
 
+            # NOTE selects TRX PHY and PRR columns from df_if_phy and sets TRX PHY as index before converting the frame
+            # to a dict with key = TRX PHY and value = PRR and finally appending to line_info[if_phy, payload_size, offset][sir]
             line_info[if_phy, payload_size, offset][sir].update(df_if_phy[["TRX PHY","PRR"]].set_index("TRX PHY")["PRR"].to_dict())
-
+   
+    # REVIEW removing else clause allows other csv files in directory
     else:
         raise ValueError("CSV filename %s incorrectly formatted" % csv_file)
 
+# NOTE loops over each (if_phy, payload_size, sir) tuple in bar_info
 for if_phy, payload_size, sir in bar_info:
     title = "Interference: " + if_phy + ", Payload: " + payload_size + ", SIR: " + sir
+    # NOTE sort by offset
     bar_info[if_phy, payload_size, sir] = dict(sorted(bar_info[if_phy, payload_size, sir].items(), key=lambda x: x[0].lower()))
     ax = pd.DataFrame(bar_info[if_phy, payload_size, sir]).T.plot(kind='bar', figsize=(10,7))
     ax.set_xlabel('Offset between TX and IF')
@@ -81,6 +92,7 @@ for if_phy, payload_size, sir in bar_info:
     plt.close()
 
 styles = ['s-', 'o-', '^-', 'x-', 'd-', 'h-']
+# NOTE loops over each (if_phy, payload_size, offset) tuple in line_info
 for if_phy, payload_size, offset in line_info:
     title = "Interference: " + if_phy + ", Payload: " + payload_size + ", Offset: " + offset
     line_info[if_phy, payload_size, offset] = dict(sorted(line_info[if_phy, payload_size, offset].items(), key=lambda x: x[0].lower()))
