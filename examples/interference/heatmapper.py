58620e8922375deb331d9fcffac3993db50ffed3
Robbe Elsas <robbe.elsas@ugent.be>
2020-01-28 16:01:27 +0100

    Updated heatmapper with basic formatting

--- examples/interference/heatmapper.py
+++ examples/interference/heatmapper.py
@@ -4,17 +4,31 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 tx_raw = pd.read_csv("/home/relsas/RIOT-benpicco/examples/interference/TX_120B_OF_-1440US_SIR_0DB.csv",header=None)
-tx_raw.columns = ["TRX PHY","IF PHY","PRR"]
+tx_raw.columns = ["TX / RX PHY\nconfiguration","Interferer PHY\nconfiguration","PRR"]
 tx_raw.replace({"SUN-OFDM 863-870MHz ":""},regex=True,inplace=True)
 
-tx_matrix = tx_raw.pivot("TRX PHY","IF PHY","PRR")
+tx_matrix = tx_raw.pivot("TX / RX PHY\nconfiguration","Interferer PHY\nconfiguration","PRR")
 
 plt.figure(figsize=(6,6))
-ax = sns.heatmap(tx_matrix,cmap="Blues",linewidths=1.2,linecolor="lightgrey",vmin=0,vmax=1.0,annot=True,square=True,cbar_kws={"shrink":0.85,"label":"PRR"})
-plt.title("Heatmap",fontsize=17)
-plt.gca().yaxis.get_label().set_fontsize(14)
-plt.gca().xaxis.get_label().set_fontsize(14)
+
+ax = sns.heatmap(tx_matrix,cmap="Blues",linewidths=1.4,linecolor="lightgrey",vmin=0,vmax=1.0,annot=True,square=True,cbar_kws={"shrink":0.8,"aspect":14})
+ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11)
+ax.tick_params(axis='both', colors='lightgrey', labelsize=11,width=1.8,length=7)
+
+plt.title("Packet Reception Ratio under 21B\nACK interference @ -1.44 ms offset",fontsize=15,pad=16)
+plt.gca().yaxis.get_label().set_fontsize(11)
+plt.gca().yaxis.labelpad = 10
+plt.gca().xaxis.get_label().set_fontsize(11)
+plt.gca().xaxis.labelpad = 10
 plt.yticks(rotation=45,va="center")
 plt.xticks(rotation=45,ha="center") 
+
+my_colors = ['g', 'g', 'r', 'r']
+
+for ticklabel, tickcolor in zip(plt.gca().get_xticklabels(), my_colors):
+    ticklabel.set_color(tickcolor)
+for ticklabel, tickcolor in zip(plt.gca().get_yticklabels(), my_colors):
+    ticklabel.set_color(tickcolor)
+
 plt.savefig("test.png",bbox_inches='tight',dpi=330)
 plt.close()
