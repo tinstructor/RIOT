5ef144ead6a8836efc4de87ce2c2571013dc8993
Robbe Elsas <robbe.elsas@ugent.be>
2020-01-29 13:43:24 +0100

    Fixed unstable font weights and ticks

--- examples/interference/heatmapper.py
+++ examples/interference/heatmapper.py
@@ -2,11 +2,8 @@ import pandas as pd
 import numpy as np 
 import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib
 
-matplotlib.font_manager._rebuild()
-
-sns.set(rc={"font.family":"sans-serif","font.sans-serif":["Fira Sans"]})
+sns.set(rc={"font.family":"sans-serif","font.weight":"regular","font.sans-serif":["Fira Sans"]})
 
 extension = "png"
 
@@ -19,9 +16,9 @@ tx_matrix = tx_raw.pivot("TX / RX PHY\nconfiguration","Interferer PHY\nconfigura
 plt.figure(figsize=(6,6))
 
 ax = sns.heatmap(tx_matrix,cmap="Reds",linewidths=1.4,linecolor="#a9a9a9",vmin=0,vmax=1.0,annot=True,square=True,cbar_kws={"shrink":0.804,"aspect":14})
-ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11)
+ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11,fontweight="regular")
 ax.collections[0].colorbar.ax.set_frame_on(True)
-ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
+ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7,tick1On=True)
 ax.collections[0].colorbar.ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
 
 for axis in ['top','bottom','left','right']:
@@ -31,10 +28,12 @@ for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_color('#a9a9a9')
     ax.collections[0].colorbar.ax.spines[axis].set_color('#a9a9a9')
 
-plt.title("Packet Reception Ratio under 21B\nACK interference @ -1.44 ms offset",fontsize=15,pad=16)
+plt.title("Packet Reception Ratio under 21B\nACK interference @ -1.44 ms offset",fontsize=15,pad=16,fontweight="regular")
 plt.gca().yaxis.get_label().set_fontsize(11)
+plt.gca().yaxis.get_label().set_weight("regular")
 plt.gca().yaxis.labelpad = 10
 plt.gca().xaxis.get_label().set_fontsize(11)
+plt.gca().xaxis.get_label().set_weight("regular")
 plt.gca().xaxis.labelpad = 10
 plt.yticks(rotation=45,va="center")
 plt.xticks(rotation=45,ha="center") 
@@ -67,9 +66,9 @@ tx_matrix = tx_raw.pivot("TX / RX PHY\nconfiguration","Interferer PHY\nconfigura
 plt.figure(figsize=(6,6))
 
 ax = sns.heatmap(tx_matrix,cmap="Reds",linewidths=1.4,linecolor="#a9a9a9",vmin=0,vmax=1.0,annot=True,square=True,cbar_kws={"shrink":0.804,"aspect":14})
-ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11)
+ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11,fontweight="regular")
 ax.collections[0].colorbar.ax.set_frame_on(True)
-ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
+ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7,tick1On=True)
 ax.collections[0].colorbar.ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
 
 for axis in ['top','bottom','left','right']:
@@ -79,10 +78,12 @@ for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_color('#a9a9a9')
     ax.collections[0].colorbar.ax.spines[axis].set_color('#a9a9a9')
 
-plt.title("Packet Reception Ratio under 21B\nACK interference @ middle offset",fontsize=15,pad=16)
+plt.title("Packet Reception Ratio under 21B\nACK interference @ middle offset",fontsize=15,pad=16,fontweight="regular")
 plt.gca().yaxis.get_label().set_fontsize(11)
+plt.gca().yaxis.get_label().set_weight("regular")
 plt.gca().yaxis.labelpad = 10
 plt.gca().xaxis.get_label().set_fontsize(11)
+plt.gca().xaxis.get_label().set_weight("regular")
 plt.gca().xaxis.labelpad = 10
 plt.yticks(rotation=45,va="center")
 plt.xticks(rotation=45,ha="center") 
@@ -115,9 +116,9 @@ tx_matrix = tx_raw.pivot("TX / RX PHY\nconfiguration","Interferer PHY\nconfigura
 plt.figure(figsize=(6,6))
 
 ax = sns.heatmap(tx_matrix,cmap="Reds",linewidths=1.4,linecolor="#a9a9a9",vmin=0,vmax=1.0,annot=True,square=True,cbar_kws={"shrink":0.804,"aspect":14})
-ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11)
+ax.collections[0].colorbar.set_label("PRR",labelpad=8,fontsize=11,fontweight="regular")
 ax.collections[0].colorbar.ax.set_frame_on(True)
-ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
+ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7,tick1On=True)
 ax.collections[0].colorbar.ax.tick_params(axis='both', colors="#a9a9a9", labelsize=11,width=1.6,length=7)
 
 for axis in ['top','bottom','left','right']:
@@ -127,10 +128,12 @@ for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_color('#a9a9a9')
     ax.collections[0].colorbar.ax.spines[axis].set_color('#a9a9a9')
 
-plt.title("Packet Reception Ratio under 21B\nACK interference @ end offset",fontsize=15,pad=16)
+plt.title("Packet Reception Ratio under 21B\nACK interference @ end offset",fontsize=15,pad=16,fontweight="regular")
 plt.gca().yaxis.get_label().set_fontsize(11)
+plt.gca().yaxis.get_label().set_weight("regular")
 plt.gca().yaxis.labelpad = 10
 plt.gca().xaxis.get_label().set_fontsize(11)
+plt.gca().xaxis.get_label().set_weight("regular")
 plt.gca().xaxis.labelpad = 10
 plt.yticks(rotation=45,va="center")
 plt.xticks(rotation=45,ha="center") 
