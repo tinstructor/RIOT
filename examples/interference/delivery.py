fb5082d1fb532429cda73422d6bfdc89cc3836f8
Robbe Elsas <robbe.elsas@ugent.be>
2020-02-27 12:02:07 +0100

    Changed plot font to Fira Sans

--- examples/interference/delivery.py
+++ examples/interference/delivery.py
@@ -6,6 +6,9 @@ from scipy import interpolate
 from numpy import nan as NaN
 import math
 import random
+from matplotlib import rc
+
+rc("font",**{"family":"sans-serif","weight":"regular","sans-serif":["Fira Sans"]})
 
 def get_payload_overlap(phy_tuple,payload_tuple):
     # NOTE phy_tuple(if_idx,trx_idx)
@@ -89,11 +92,18 @@ tx_raw["PRR"] = tx_complete.apply(lambda row: random.random(),axis=1)
 tx_complete = pd.concat([tx_complete,tx_raw])
 tx_complete.reset_index(drop=True,inplace=True)
 
+# tx_raw = tx_raw.iloc[[0]]
+# tx_raw["Interferer payload"] = 80
+# tx_raw["Interferer PHY\nconfiguration"] = "O4 MCS3"
+# tx_raw["TX / RX PHY\nconfiguration"] = "O3 MCS1"
+# tx_raw["PRR"] = random.random()
+# # print(tx_raw)
+# tx_complete = pd.concat([tx_complete,tx_raw])
+# tx_complete.reset_index(drop=True,inplace=True)
+
 phy_names = {"O4 MCS2":2,"O4 MCS3":3,"O3 MCS1":4,"O3 MCS2":5}
 tx_complete["Payload overlap"] = tx_complete.apply(lambda row: get_payload_overlap((phy_names[row["Interferer PHY\nconfiguration"]],phy_names[row["TX / RX PHY\nconfiguration"]]),(row["Interferer payload"],row["TX / RX payload"])),axis=1)
 tx_complete["Weighted PRR"] = tx_complete["PRR"] * tx_complete["Payload overlap"]
-
-# print(tx_complete)
 
 trx_phy_list = sorted(tx_complete["TX / RX PHY\nconfiguration"].unique().tolist())
 
@@ -102,24 +112,30 @@ coord = {0:(0,0),1:(0,1),2:(1,0),3:(1,1)}
 cmap = cm.get_cmap('Spectral')
 
 for index, trx_phy in enumerate(trx_phy_list):
-    # print(tx_complete.loc[tx_complete["TX / RX PHY\nconfiguration"] == trx_phy])
     dfp = tx_complete.loc[tx_complete["TX / RX PHY\nconfiguration"] == trx_phy].pivot(index="Payload overlap", columns="Interferer PHY\nconfiguration", values="PRR")
-    # print(dfp)
     dfp_i = tx_complete.loc[tx_complete["TX / RX PHY\nconfiguration"] == trx_phy].pivot(index="Payload overlap", columns="Interferer PHY\nconfiguration", values="PRR")
-    for i in np.arange(dfp_i.index.min(),dfp_i.index.max(),0.0025):
+    for i in np.arange(dfp_i.index.min(),dfp_i.index.max(),0.001):
         if not i in dfp_i.index:
             dfp_i.loc[i] = [NaN,NaN,NaN,NaN]
-    dfp_i = dfp_i.sort_index().interpolate(method="quadratic",limit_area="inside")
+    dfp_i = dfp_i.sort_index().interpolate(method="cubic",limit_area="inside")
 
     dfp.plot(ax=axes[coord[index][0],coord[index][1]],marker='x',markeredgewidth=1.8,markersize=8,linestyle="None",legend=False,colormap=cmap)
     ax = dfp_i.plot(ax=axes[coord[index][0],coord[index][1]],linewidth=1.5,legend=False,colormap=cmap)
-    axes[coord[index][0],coord[index][1]].set_xticks(np.arange(0.0,1.0,0.02 if coord[index][1] == 0 else 0.04).tolist())
-    axes[coord[index][0],coord[index][1]].set_xlim(dfp_i.index.min()-0.01,dfp_i.index.max()+0.02)
+    tick_offset = (0.02 if coord[index][1] == 0 else 0.04)
+    axes[coord[index][0],coord[index][1]].set_xticks(np.arange(round(dfp_i.index.min(),2),round(dfp_i.index.max(),2)+tick_offset,tick_offset).tolist())
+    axes[coord[index][0],coord[index][1]].set_xlim(round(dfp_i.index.min(),2)-0.01,round(dfp_i.index.max(),2) + 0.01)
     axes[coord[index][0],coord[index][1]].set_ylim(-0.05,1.05)
     axes[coord[index][0],coord[index][1]].set_ylabel("PRR")
     axes[coord[index][0],coord[index][1]].title.set_text("TX / RX PHY: " + trx_phy)
+    axes[coord[index][0],coord[index][1]].title.set_fontsize(14)
+    axes[coord[index][0],coord[index][1]].title.set_weight("regular")
+    axes[coord[index][0],coord[index][1]].xaxis.get_label().set_fontsize(12)
+    axes[coord[index][0],coord[index][1]].xaxis.get_label().set_weight("regular")
+    axes[coord[index][0],coord[index][1]].yaxis.get_label().set_fontsize(12)
+    axes[coord[index][0],coord[index][1]].yaxis.get_label().set_weight("regular")
 
 handles, labels = ax.get_legend_handles_labels()
 fig.legend(handles[4:8], labels[4:8], loc="lower center",ncol=4)
 
+plt.subplots_adjust(wspace=0.15,hspace=0.35,top=0.9,left=0.1,right=0.9)
 plt.show()
