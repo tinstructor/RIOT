3aabf86c9d68000c4a0ade77b4536050b5973b7f
Robbe Elsas <robbe.elsas@ugent.be>
2020-05-06 13:15:52 +0200

    Attplot now with seaborn regplot

--- examples/interference/attplot.py
+++ examples/interference/attplot.py
@@ -26,18 +26,18 @@ for trx_payload_size in trx_payload_sizes:
     tx_raw = tx_raw[tx_raw["PRR"] > 0.05]
     tx_raw.drop_duplicates(inplace=True)
     tx_raw.reset_index(drop=True,inplace=True)
-    tx_matrix = tx_raw.pivot_table(index=["RSSI"],columns=["PHY"],values=["PRR"])
-    ax = tx_matrix.plot(linestyle="None",marker="x")
-    ax.set_ylabel("PRR")
-    ax.yaxis.get_label().set_fontsize(12)
-    ax.yaxis.get_label().set_weight("regular")
-    ax.set_xlabel("RSSI")
-    ax.xaxis.get_label().set_fontsize(12)
-    ax.xaxis.get_label().set_weight("regular")
-    ax.legend(title="")
-    ax.title.set_text("PRR in function of average\nRSSI for %dB PSDU" % trx_payload_size)
-    ax.title.set_fontsize(14)
-    ax.title.set_weight("regular")
+
+    phy_list = tx_raw["PHY"].unique()
+    for index,phy in enumerate(phy_list):
+        colors = ["C0","C1","C2","C3","C4","C5","C6"]
+        sns.regplot(x="RSSI",y="PRR",data=tx_raw[tx_raw["PHY"] == phy],order=2,ci=None,color=colors[index])
+    plt.title("PRR in function of average\nRSSI for %dB PSDU" % trx_payload_size,fontsize=14,fontweight="regular")
+    plt.gca().yaxis.get_label().set_fontsize(12)
+    plt.gca().yaxis.get_label().set_weight("regular")
+    plt.gca().xaxis.get_label().set_fontsize(12)
+    plt.gca().xaxis.get_label().set_weight("regular")
+    plt.legend(phy_list,title="")
+
     image_file = "TX_%dB.%s" % (trx_payload_size,extension)
     plt.savefig(image_file,bbox_inches='tight',dpi=330,transparent=transparent_flag)
     plt.close()
